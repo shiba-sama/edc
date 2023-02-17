89e7e217649e72050fd8227a1d78e6eb1952bcff
meow <meow@>
2023-02-17 00:54:20 -0800

    update

--- src/math.ts
+++ src/math.ts
@@ -169,3 +169,13 @@ export function randomInts(n, bits=32) {
       default: throw Error(`Unsupported number of bits: ${bits}`)
    }
 }
+
+/** Generates random alphanumeric string. */
+export function randomString(length:number) {
+   const bytes = new Uint8Array(Math.ceil(length / 2))
+   crypto.getRandomValues(bytes)
+   return Array
+      .from(bytes, byte => byte.toString(16).padStart(2, "0"))
+      .join("")
+      .substring(0, length)
+}
